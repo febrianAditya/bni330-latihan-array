fd437cd1743a774f983801a6cf88eabdd4a01318
FebrianAditya <febrian.aditya@student.upi.edu>
2025-05-17 07:22:38 +0700

    soal latihan

---
+++ soal.js
@@ -0,0 +1,65 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+let resultOne
+// Jumlahkan semua angka genap dalam array berikut
+
+
+const numbersTwo = [1, 2, 3, 4, 5, 6, 7, 8];
+let resultTwo
+//Tambahkan semua angka ganjil dalam array dan kembalikan hasilnya.
+
+
+let students = ["Tito", "arY", "Bostang"]
+let resultThree
+// Ubah semua huruf kecil dalam array menjadi huruf besar
+
+
+const people = [
+    { name: 'Andi', age: 17 },
+    { name: 'Tito Alexsta', age: 20 },
+    { name: 'Bostang', age: 22 },
+    { name: 'Cici', age: 17 }
+];
+// Ambil hanya nama-nama orang yang umurnya di atas 18 tahun.
+// Ambil hanya orang yang panjang namanya lebih dari 5
+
+
+const items = [
+    { name: 'Book', price: 10000 },
+    { name: 'Pen', price: 5000 },
+    { name: 'Pencil', price: 3000 },
+];
+// Hitung total harga semua barang
+  
+
+const numbersThree = [1, 2, 2, 3, 4, 4, 5];
+//Ambil hanya angka unik dari array (hapus duplikat).
+
+
+const products = [
+    { name: 'Laptop', price: 15000000 },
+    { name: 'Phone', price: 8000000 },
+    { name: 'Monitor', price: 3000000 },
+];
+// Ambil nama produk dengan harga tertinggi
+
+
+const words = ['Belajar', 'array', 'itu', 'menyenangkan'];
+// Buat kalimat dari array kata.
+
+
+const names = ['Aldo', 'Budi', 'Caca', 'Dinda', 'Eka', 'Fajar'];
+// Ambil hanya 3 data pertama yang mengandung kata 'a' pada nama.
+
+
+const namesTwo = ['Ani', 'Budi', 'Citra']; // 12 karakter
+// Hitung total karakter dari semua nama dalam array.
+
+
+const peopleTwo = [
+    { name: 'Tono', age: 22 },
+    { name: 'Tini', age: 19 },
+    { name: 'Tara', age: 25 },
+];
+// Output: ['Tono (22)', 'Tara (25)']
+// Ubah array objek ke array string dengan format 'Nama (Umur)' dan hanya yang umurnya di atas 21.
+
